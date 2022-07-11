a1123165f689b708fe3c9817ac15b0188cac0b73
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-11 17:23:05 -0500

    Add properties model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_220243) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_221440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "address"
+    t.float "price"
+    t.float "maintenance"
+    t.integer "property_type"
+    t.boolean "pets"
+    t.text "about"
+    t.integer "bedrooms"
+    t.integer "bathrooms"
+    t.integer "area"
+    t.integer "operation_type"
+    t.bigint "user_id", null: false
+    t.boolean "is_active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address"], name: "index_properties_on_address", unique: true
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "type"
@@ -27,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_220243) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "properties", "users"
 end
