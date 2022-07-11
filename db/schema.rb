c6ef49a7dd472f5f1e5a7f0ab3dac9f5e52fdb69
Diego Manrique <diego.manrique.m@uni.pe>
2022-07-11 18:52:09 -0500

    Add validates and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_222748) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_234507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_222748) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "type"
+    t.integer "user_type"
     t.string "name"
     t.string "email"
     t.integer "phone"
