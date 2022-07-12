37803a1d2c50789d7306bbe795deb872bbad7000
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-11 19:21:34 -0500

    Ran rubocop

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,12 +3,12 @@ class Property < ApplicationRecord
   has_many_attached :photos
 
   validates :address, presence: true, uniqueness: true
-  validates :price, numericality:{ less_than: 99999999.99}
-  validates :maintenance, numericality:{ less_than: 99999999.99}, allow_nil: true
+  validates :price, numericality: { less_than: 99_999_999.99 }
+  validates :maintenance, numericality: { less_than: 99_999_999.99 }, allow_nil: true
   enum property_type: { apartment: 0, house: 1 }
   validates :pets, presence: true
   validates :about, length: { maximum: 300 }, presence: true
-  enum operation_type: { rent: 0, sale: 1 } 
+  enum operation_type: { rent: 0, sale: 1 }
   validates :bedrooms, presence: true
   validates :bathrooms, presence: true
   validates :area, presence: true
