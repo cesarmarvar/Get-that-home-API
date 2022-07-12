9a0564cee5f138b3439e6dcab3d1beb13e1b4193
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-11 22:49:14 -0500

    Added index method to buyer propierties controller

--- app/controllers/buyer_properties_controller.rb
+++ app/controllers/buyer_properties_controller.rb
@@ -1,2 +1,15 @@
 class BuyerPropertiesController < ApplicationController
+  before_action :authorize
+
+  def index 
+    if current_user.user_type == 0
+      render json: current_user.properties
+    else
+      render json: current_user.buyer_properties
+    end
+  end
+
+  
+
+
 end
