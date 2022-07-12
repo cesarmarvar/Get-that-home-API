48b99cf38ee8fd8f3707d4931ce81e66e24916bf
emachipana <emachipana0102@gmail.com>
2022-07-12 01:12:42 -0500

    add: create buyer property

--- app/controllers/buyer_properties_controller.rb
+++ app/controllers/buyer_properties_controller.rb
@@ -2,11 +2,26 @@ class BuyerPropertiesController < ApplicationController
   before_action :authorize
 
   def index
-    if current_user.user_type.zero?
+    if current_user.user_type == "landlord"
       render json: current_user.properties
     else
-      properties = current_user.buyer_properties.map(&:property)
+      properties = current_user.buyer_properties
       render json: properties
     end
+  end
+
+  def create
+    @buyer_property = current_user.buyer_properties.new(buyer_params)
+    if @buyer_property.save
+      render json: @buyer_property, status: :created
+    else
+      render json: { errors: @buyer_property.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.permit(:property_id, :property_status)
   end
 end
