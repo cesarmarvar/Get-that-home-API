b4cf288aff7078f275f8a941802e95f5dbeaef59
emachipana <emachipana0102@gmail.com>
2022-07-12 11:12:47 -0500

    ADD: update and destroy

--- app/controllers/buyer_properties_controller.rb
+++ app/controllers/buyer_properties_controller.rb
@@ -1,11 +1,12 @@
 class BuyerPropertiesController < ApplicationController
   before_action :authorize
+  before_action :set_properties, only: %i[update destroy]
 
   def index
     if current_user.user_type == "landlord"
       render json: current_user.properties
     else
-      properties = current_user.buyer_properties
+      properties = current_user.buyer_properties.map{ |e| { id: e.id, property_status: e.property_status, property: e.property } }
       render json: properties
     end
   end
@@ -19,7 +20,24 @@ class BuyerPropertiesController < ApplicationController
     end
   end
 
+  def update
+    if @saved_property.update(buyer_params)
+      render json: @saved_property
+    else
+      render json: { errors: @saved_property.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @saved_property.destroy
+    head :ok
+  end
+
   private
+
+  def set_properties
+    @saved_property = current_user.buyer_properties.find(params[:id])
+  end
 
   def buyer_params
     params.permit(:property_id, :property_status)
