7752bdff6f2ea40f45e8d51eef517b3bd16771dd
emachipana <emachipana0102@gmail.com>
2022-07-12 00:20:25 -0500

    ADD: set user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authorize, only: :create
+  before_action :set_user, only: %i[show]
 
   def create
     @user = User.new(user_params)
@@ -19,12 +20,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: current_user
+    render json: @respond_user
   end
 
   def update
     if current_user.update(user_params)
-      render json: current_user, status: :ok
+      respond_user = {
+        user_type: current_user.user_type,
+        name: current_user.name,
+        email: current_user.email,
+        phone: current_user.phone
+      }
+      render json: respond_user, status: :ok
     else
       render json: { errors: current_user.errors }, status: :unprocessable_entity
     end
@@ -36,6 +43,15 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @respond_user = {
+      user_type: current_user.user_type,
+      name: current_user.name,
+      email: current_user.email,
+      phone: current_user.phone
+    }
+  end
 
   def user_params
     params.permit(:user_type, :name, :email, :phone, :password)
